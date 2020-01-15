315e3612a357a5714dc14d61a1e06a6b8cf2ced4
Niranjay <niranjay.bhosale@gmail.com>
2020-01-15 18:00:55 +0530

    Update fibonacciSeries.py

--- fibonacciSeries.py
+++ fibonacciSeries.py
@@ -1,13 +1,17 @@
 def fib(n):
     first = 0
     second = 1
-    print(first, second, end=" ")
 
-    for i in range(n):
-        next = first + second
-        print(next, end=" ")
-        first = second
-        second = next
+    if n==1:
+        print(first)
+    else:
+        print(first, second, end=" ")
+
+        for i in range(n-2):
+            next = first + second
+            print(next, end=" ")
+            first = second
+            second = next
 
 
 n = int(input("Enter how many numbers of the series you want:"))
